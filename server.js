932ab85ba3094869a7d7a9f8f716873e7943a646
caseylholston <caseylholston@gmail.com>
2017-05-03 21:22:10 -0400

    Added process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
